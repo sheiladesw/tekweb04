ffa4f8dcc6a01484bd12ccf59dd304410a65ea1c
Sheila Deswita Ariyanto <97265445+sheiladesw@users.noreply.github.com>
2022-05-21 11:09:25 +0700

    update datanya

--- js/main.js
+++ js/main.js
@@ -10,15 +10,15 @@ Vue.createApp({
                     'j':"About" 
                 },
                 {
-                    'j':"Artikel" 
+                    'j':"project" 
                 },
                 {
-                    'j':"Portfolio" 
+                    'j':"Artikel" 
                 }
             ]
         },
         desc1: "Holaaa, I'm ",
-        desc2: "Let Me Show You A Few Things About Me. So, Welcome To My Blog",
+        desc2: "Let Me Show You A Few Things About Me. So, Welcome To My Website",
         socmed: [
             {
                 'judul': "Instagram",
@@ -39,65 +39,39 @@ Vue.createApp({
 
         //Bagian Tentang
         tentang: {
-            judul1: "Tentang ",
-            judul2: "Saya",
-            desc: "Hallo, perkenalkan nama saya Sheila Deswita Ariyanto, biasa dipanggil sela. Saya adalah salah satu mahasiswi semester 4 program studi sistem informasi. Saat ini saya sedang mempelajari cara membuat website pribadi sebagai portofolio dari tugas mata kuliah teknologi web lanjut. ",
-
+          judul1: "Tentang ",
+          judul2: "Saya",
+          desc: "Hallo, perkenalkan nama saya Sheila Deswita Ariyanto, biasa dipanggil sela. Saat ini saya sedang menempuh pendidikan di Universitas Ahmad Dahlan Yogyakarta, Program studi sistem informasi. Saya merupakan mahasiswi semester 4 yang sedang mempelajari cara membuat website pribadi sebagai portofolio dari tugas mata kuliah teknologi web lanjut. ",
         },
 
-        //Bagian tabel
-        tabel: {
-            nama: "Tabel Keterampilan",
-            judul: ["No.","Keterampilan","Level"],
-            datanya: [
-                {
-                    "j":"HTML",
-                    "a":"Advance"
+        // bagian skill
+        skill: "My Skill", 
+        skill01: "M.word - Advanced",
+        skill02: "Html - Upper Intermediate",
+        skill03: "Css Intermediate",
+        skill04: "Figma - Advanced",
+        skill05: "Python - Intermediate",
+        skill06: "SPSS - Intermediate",
+        skill07: "SQLYog - Advanced",
+        skill08: "Qgis - Advanced",
 
-                },
-                {
-                   "j": "CSS",
-                   "a":"intermediatte"
-                },
-                {
-                    "j":"Vue-js",
-                    "a":"Intermediatte"
-                },
-                {
-                    "j":"Microsoft Word",
-                    "a":"Advance"
 
-                },
-                {
-                    "j":"Microsoft Power Point",
-                    "a":"Advance"
-                },
-                {
-                    "j":"Microsoft Excel",
-                    "a":"Advance"
-                },
-                {
-                    "j":"javascript",
-                    "a":"Intermediatte"
-                },
-                {
-                    "j":"Java",
-                    "a":"Intermediatte"
-                }
-            ],
+        // Bagian Project
+        project : {
+          judul1: "My",
+          judul2: "Project"
         },
-
 
         //Bagian Artikel
         artikel: {
-            judul: "Artikel",
-            list_artikel: [],
-            
+          judul: "Artikel",
+          list_artikel: [],
         },
-        nama_artikel: null,
-        
-      };
-    },
+          nama_artikel: null,
+            
+          };
+        },
+    
     methods: { //tempat menambahkan fungsi-fungsi
         ambilListArtikel()
         {
@@ -114,11 +88,11 @@ Vue.createApp({
             });
         },
         ambilDataMarkdown()
-      {               
-        const queryString = window.location.search;
-        const urlParams = new URLSearchParams(queryString);
-        const artikel = urlParams.get('nama_artikel');      
-        var converter = new showdown.Converter();
+    {               
+      const queryString = window.location.search;
+      const urlParams = new URLSearchParams(queryString);
+      const artikel = urlParams.get('nama_artikel');      
+      var converter = new showdown.Converter();
         axios
           .get(
             src="../artikel/"+artikel
@@ -137,4 +111,67 @@ Vue.createApp({
       this.ambilDataMarkdown()
     },
   }).mount("#app");
+
+  //Script untuk menghilangkan navigasi collapse jika dikilk akan hilang -->
+    $('.js-scroll-trigger').click(function () {
+      $('.navbar-collapse').collapse('hide');
+    });
+
+ //js nama pada home
+  const typedTextSpan = document.querySelector(".typed-text");
+  const cursorSpan = document.querySelector(".cursor");
+
+  const textArray = ["Sheila"];
+  const typingDelay = 100;
+  const erasingDelay = 100;
+  const newTextDelay = 2500; 
+  let textArrayIndex = 0;
+  let charIndex = 0;
+
+  function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+      if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+      typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+      charIndex++;
+      setTimeout(type, typingDelay);
+    }
+    else {
+      cursorSpan.classList.remove("typing");
+      setTimeout(erase, newTextDelay);
+    }
+  }
+
+  function erase() {
+    if (charIndex > 0) {
+      if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+      typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+      charIndex--;
+      setTimeout(erase, erasingDelay);
+    }
+    else {
+      cursorSpan.classList.remove("typing");
+      textArrayIndex++;
+      if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+      setTimeout(type, typingDelay + 1100);
+    }
+  }
+
+  document.addEventListener("DOMContentLoaded", function () { // On DOM Load initiate the effect
+    if (textArray.length) setTimeout(type, newTextDelay + 250);
+  });
   
+  // navbar scroll -->
+    document.addEventListener("scroll", function () {
+      const navbar = document.querySelector("nav");
+
+      if (this.body.scrollTop > 1 || this.documentElement.scrollTop > 1) {
+        navbar.classList.add("nav-scrolled", "fixed-top");
+      } else {
+        navbar.classList.remove("nav-scrolled", "fixed-top");
+      }
+    });
+
+    
+  
+
+    
